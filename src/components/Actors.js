aa7517fee3e518da764d1739632760b152af6ea3
thexyman <makdaa1@gmail.com>
2018-11-05 13:29:42 +0000

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { actors } from '../data';
 
-const Actors = () => {
-  return (
-    <div>
-      {/*{code here}*/}
+const displayActors = (actor, idx) => {
+  return(
+    <div key={idx}>
+      <h3>{actor.name}</h3>
+      <ul>
+        {
+          actor.movies.map((movie, idx) => <li key={idx} >{movie}</li>)
+        }
+      </ul>
     </div>
-  );
-};
+  )
+}
+
+const Actors = () => 
+
+    <div>
+      <h1>Actors Page</h1>
+      {
+        actors.map((actor, idx) => displayActors(actor, idx))
+      }
+    </div>
+
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import { movies } from '../data';
 
-const Movies = () => {
+const displayMovies = (movie, idx) => {
   return (
-    <div>
-        {/*{code here}*/}
-    </div>
-  );
-};
+    <div key={idx} >
+          <h3>{movie.title}</h3>
+          <p>Moive Length: {movie.time}</p>
+          <ul>
+            {
+              movie.genres.map((genre, idx) => <li key={idx} >{genre}</li>)
+            }
+          </ul>
+    </div> 
+  )
+}
+
+
+const Movies = () => 
+
+  <div>
+    <h1>Movies Page</h1>
+    {movies.map((movie, idx) => displayMovies(movie, idx))}
+  </div>
+  
 
 export default Movies;
+
+
+
