636562220d436ed65ea14caba013c8a007ae01f5
braj011 <bhagyaraj01@gmail.com>
2018-11-07 23:16:42 +0000

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,9 +4,26 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+       <div>
+          <h1>Directors Page</h1>
+          {directors.map((director, index) => (
+          <div key={index}>
+              <h3>Name: {director.name}</h3>
+              <p>Movies:</p>
+              <ul>
+                {director.movies.map((movie, index) => (
+                  // use index when items don't have stable ids
+                    // https://reactjs.org/docs/lists-and-keys.html
+                <li key={index}>{movie}</li>
+                ))}
+              </ul>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
 export default Directors
+
+
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,9 @@ import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom';
+// import ReactDOM from 'react-dom';
+
+
 import NavBar from '../components/NavBar';
 import Home from '../components/Home';
 import Actors from '../components/Actors';
@@ -13,7 +16,13 @@ import Movies from '../components/Movies';
 const App = (props) => {
   return (
     <Router>
-      {/*{code here}*/}
+      <div>
+        <NavBar />
+        <Route path="/" render={Home} />
+        <Route path="/movies" render={Movies} />
+        <Route path="/directors" render={Directors} />
+        <Route path="/actors" render={Actors} />
+      </div>
     </Router>
   );
 };
